53a4c7b2546d167bb0975522485ef800a99faa21
Marcelo Jarretta <marcelo.jarretta@teslatechsolutions.com>
2020-12-30 00:11:12 -0300

    Final version with correct files

--- plot1.R
+++ plot1.R
@@ -3,3 +3,5 @@ load("data.Rdata")
 png("plot1.png",width=480,height=480,units="px")
 hist(data$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
+
+rm(data)
